d13e75fc0358559d567398664e257d5f211b4cf3
GREESHMA-CHACKO <160748918+GREESHMA-CHACKO@users.noreply.github.com>
2024-02-22 19:27:39 +0530

    program for even or odd

---
+++ EvenOodd.java
@@ -0,0 +1,18 @@
+class EvenOodd
+{
+public static void main(String args[])
+{
+int n=100;
+for(int i=0;i<n;i++)
+{
+if(n%2==0)
+{
+evencount++;
+}
+else
+{
+oddcount++;
+}
+System.out.println("The number is even"+evancount++);
+System.out.println("The number is odd"+oddcount);
+}}
