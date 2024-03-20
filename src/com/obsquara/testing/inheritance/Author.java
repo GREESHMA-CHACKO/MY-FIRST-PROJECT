c83192496315bff959df53129d0454227653c64b
Dell <Dell@DESKTOP-4SB8VJN>
2024-03-20 22:26:56 +0530

    author

---
+++ src/com/obsquara/testing/inheritance/Author.java
@@ -0,0 +1,16 @@
+package com.obsquara.testing.inheritance;
+
+public class Author {
+	String authorName;
+	int age;
+	String place;
+	public Author(String authorName, int age, String place) {
+		super();
+		this.authorName = authorName;
+		this.age = age;
+		this.place = place;
+	}
+	
+	
+
+}
---
+++ src/org/mes/training/array/Address.java
@@ -0,0 +1,12 @@
+package org.mes.training.array;
+
+public class Address {
+	String housename;
+	String city;
+	int houseno;
+	Student no;
+	
+	
+	
+
+}
